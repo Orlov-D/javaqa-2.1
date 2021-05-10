8914f83c63dba52e345d5be826675cb8a7e47000
Orlov Dmitry <orlovdimas92@mail.ru>
2021-05-10 22:05:13 +0300

    added all files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main   {
+    public static void main(String[] args) {
+        int balance =2_000_000_000;
+        int sum_of_transfer=500_000_000;
+        int balance_after=balance+sum_of_transfer;
+        System.out.println("After transfer "+balance_after);
+    }
+}
